7865ae96ddae6863d1fe65e5179ca80aa8c2485b
Edgar <57034089+Edw125@users.noreply.github.com>
2020-04-05 16:29:09 +0300

    Added mac changer and network scanner

---
+++ MAC_changer.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env_python
+import subprocess
+import optparse
+import re
+import random
+
+
+# ---------------------------------
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC-address")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC-address")
+    return parser.parse_args()
+
+
+# ---------------------------------
+def change_mac(interface, mac):
+    print("Changing MAC-address for " + interface + " to " + mac)
+    subprocess.call(["sudo", "ifconfig", interface, "down"])
+    subprocess.call(["sudo", "ifconfig", interface, "hw", "ether", mac])
+    subprocess.call(["sudo", "ifconfig", interface, "up"])
+
+
+# ---------------------------------
+def get_current_mac(interface):
+    ifconfig_result = subprocess.check_output(["sudo", "ifconfig", interface])
+    mac_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result.decode('utf-8'))
+    if mac_result:
+        return mac_result.group(0)
+    else:
+        print("Could not read MAC-address")
+
+
+# ---------------------------------
+def get_current_interface():
+    ifconfig_result = subprocess.check_output(["sudo", "ifconfig"])
+    interface_result = re.findall(r"\b\w+(?=:\s)", ifconfig_result.decode('utf-8'))
+    if interface_result:
+        return ", ".join(interface_result)
+    else:
+        print("Could not read interfaces")
+
+
+# ---------------------------------
+def get_random_mac():
+    mac = [0x00, 0x16, 0x3e,
+           random.randint(0x00, 0x7f),
+           random.randint(0x00, 0xff),
+           random.randint(0x00, 0xff)]
+    return ':'.join(map(lambda x: "%02x" % x, mac))
+
+
+# ---------------------------------
+(options, arguments) = get_arguments()
+print("Interfaces of this machine: " + get_current_interface())
+if options.interface is None:
+    options.interface = input("Input interface: ")
+current_mac = get_current_mac(options.interface)
+print("Current MAC-address: " + str(current_mac))
+
+# ----------------------------------
+answer = input("Do you want to change MAC-address to random? (yes/no): ")
+if answer == 'yes':
+    options.new_mac = get_random_mac()
+    change_mac(options.interface, options.new_mac)
+elif answer == 'no':
+    if options.new_mac is None:
+        options.new_mac = input("Input MAC-address: ")
+    change_mac(options.interface, options.new_mac)
+
+# ----------------------------------
+current_mac = get_current_mac(options.interface)
+if current_mac == options.new_mac:
+    print("MAC-address was successfully changed to " + current_mac)
+else:
+    print("MAC-address did not get changed")
---
+++ network_scanner.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env_python
+from scapy.layers.l2 import ARP, Ether
+from scapy.sendrecv import srp
+import optparse
+
+
+def scan(ip):
+    arp_request = ARP(pdst=ip)
+    broadcast = Ether(dst="ff:ff:ff:ff:ff:ff")
+    package = broadcast / arp_request
+    answered = srp(package, timeout=1)[0]
+    clients = list()
+    for i in answered:
+        clients_param = {"ip": i[1][1].psrc, "mac": i[1][1].hwsrc}
+        clients.append(clients_param)
+    return clients
+
+
+def print_clients(clients_list):
+    print("IP\t\t\t\tMAC-address")
+    print("-" * 50)
+    for element in clients_list:
+        print(element['ip'] + '\t' * 3 + element['mac'])
+
+
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-t", "--target", dest="ip", help="Target IP-address or range of IP-addresses")
+    (options, arguments) = parser.parse_args()
+    return options
+
+
+opt = get_arguments()
+if opt.ip is None:
+    opt.ip = input("Input IP-address or range of IP-addresses: ")
+print_clients(scan(opt.ip))
